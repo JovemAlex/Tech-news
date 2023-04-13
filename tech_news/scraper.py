5119d13bf50c9c88a62af8c0bca7436f93cd0f85
Alex <alexsandrolima991@hotmail.com>
2023-04-13 18:25:20 -0300

    (feat): Add scrape news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -41,7 +41,26 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css('link[rel="canonical"]::attr(href)').get()
+    title = selector.css(".entry-title::text").get().strip()
+    timestamp = selector.css(".meta-date::text").get()
+    writer = selector.css(".url.fn.n::text").get()
+    reading_time = selector.css(".meta-reading-time::text").get().split()[0]
+    summary = "".join(
+        selector.css(".entry-content > p:nth-of-type(1) ::text").getall()
+    ).strip()
+    category = selector.css("span.label::text").get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": int(reading_time),
+        "summary": summary,
+        "category": category,
+    }
 
 
 # Requisito 5
