c028689ed2b18c4ac5fac7ff7e04ac34044feb6b
Alex <alexsandrolima991@hotmail.com>
2023-04-14 12:55:17 -0300

    (feat): Add search by date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,5 @@
 from tech_news.database import search_news
-# from datetime import datetime
+from datetime import datetime
 
 
 # Requisito 7
@@ -13,7 +13,21 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        data_struct = datetime.strptime(date, "%Y-%m-%d")
+        data_brasil = data_struct.strftime("%d/%m/%Y")
+
+        news_by_date = search_news({"timestamp": data_brasil})
+
+        if news_by_date:
+            return [(news["title"], news["url"]) for news in news_by_date]
+        else:
+            return []
+
+    except ValueError:
+        raise ValueError("Data inválida")
+
+
 
 
 # Requisito 9
