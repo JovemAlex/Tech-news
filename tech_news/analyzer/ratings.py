30657b26eeac1071fa67b363ff884918289931ae
Alex <alexsandrolima991@hotmail.com>
2023-04-14 21:18:24 -0300

    feat: Add top 5 Categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,21 @@
+from tech_news.database import search_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    top = []
+    news_filtered = search_news({})
+    if news_filtered:
+        [top.append(news["category"]) for news in news_filtered]
+        contagem = Counter(top)
+        return sorted(
+            contagem.keys(),
+            key=lambda categoria: (-contagem[categoria], categoria),
+        )[:5]
+    else:
+        return []
+
+
+test = top_5_categories()
+print(test)
